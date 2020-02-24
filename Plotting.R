8cba8f4543725b660560709e3cb0eae5af199eb6
carljpearson <carl.j.pearson@gmail.com>
2020-02-24 09:28:21 -0600

    Added plot for special repot

--- Plotting.R
+++ Plotting.R
@@ -246,18 +246,19 @@ ggsave("/Users/carlpearson/Documents/r_github/openshift_alerting/plots/channel.p
 
 df_long %>%
   ggplot(aes(x=alert_name,y=conf ,fill=alert_name)) +
-  geom_violin(alpha=.5,color="white")+
+  #geom_violin(alpha=.5,color="white")+
   stat_summary(fun=mean,size=1) +
   ggthemes::theme_tufte(base_family = "sans",base_size = 15) +
   theme(
     legend.position = "none",
     axis.title.y = element_blank()
   ) + 
+  scale_y_continuous(breaks = c(1,4,7)) +
   coord_flip(ylim=c(1,7)) +
   labs(y="Confidence",
        subtitle = "Point = mean, line = SD, color = response distributions")
 
-ggsave("/Users/carlpearson/Documents/r_github/openshift_alerting/plots/conf_viol2.png",width = 12,height = 8)
+ggsave("/Users/carlpearson/Documents/r_github/openshift_alerting/plots/conf_viol2_alt.png",width = 12,height = 8)
 
 
 #plotting heatmaps
